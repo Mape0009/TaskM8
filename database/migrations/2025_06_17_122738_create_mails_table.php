a36353545e4dd24017f0c89219b3fbd906a6fdd8
Mape0009 <mape0009@edu.mercantec.dk>
2025-06-17 14:37:36 +0200

    Created rough models and migrations

---
+++ database/migrations/2025_06_17_122738_create_mails_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mails', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('subject');
+            $table->text('body');
+            $table->unsignedBigInteger('senderId');
+            $table->unsignedBigInteger('recipientId');
+            $table->dateTime('sentAt');
+            $table->foreign('senderId')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('recipientId')->references('id')->on('users')->onDelete('cascade');
+            $table->unique(['senderId', 'recipientId', 'sentAt'], 'mail_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mails');
+    }
+};
