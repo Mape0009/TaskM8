258f4fda61d0f97d58b41781672e39f9eb6345d4
Mape0009 <mape0009@edu.mercantec.dk>
2025-06-18 09:12:25 +0200

    Temporary controllers

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,8 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\EventParticipant;
+use App\Models\Event;
 
 class EventController extends Controller
 {
-    //
+    public function index()
+    {
+        $events = Event::all();
+        return response()->json($events);
+    }
+
+    public function show($id)
+    {
+        $event = Event::findOrFail($id);
+        return response()->json($event);
+    }
+
+    public function create(Request $request)
+    {
+        $event = new Event();
+        $event->eventName = $request->input('eventName');
+        $event->startDate = $request->input('startDate');
+        $event->endDate = $request->input('endDate');
+        $event->description = $request->input('description');
+        $event->Participants = $request->input('Participants', []);
+        $event->save();
+        return response()->json($event, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $event = Event::findOrFail($id);
+        $event->eventName = $request->input('eventName');
+        $event->startDate = $request->input('startDate');
+        $event->endDate = $request->input('endDate');
+        $event->description = $request->input('description');
+        $event->save();
+        return response()->json($event);
+    }
+
+    public function delete($id)
+    {
+        $event = Event::findOrFail($id);
+        $event->delete();
+        return response()->json(null, 204);
+    }
 }
--- app/Http/Controllers/EventParticipantController.php
+++ app/Http/Controllers/EventParticipantController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\EventParticipant;
 
 class EventParticipantController extends Controller
 {
-    //
+    public function index()
+    {
+        $participants = EventParticipant::all();
+        return response()->json($participants);
+    }
+
+    public function show($id)
+    {
+        $participant = EventParticipant::findOrFail($id);
+        return response()->json($participant);
+    }
+
+    public function delete($id)
+    {
+        $participant = EventParticipant::findOrFail($id);
+        $participant->delete();
+        return response()->json(['message' => 'Participant deleted successfully']);
+    }
 }
