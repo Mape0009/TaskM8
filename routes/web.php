d5eb99a1238b5be57d9f282f8ae862e5c0ca2230
Mape0009 <mape0009@edu.mercantec.dk>
2025-06-18 10:29:22 +0200

    Update Web
    Routes for all current controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventParticipantController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\MailController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// User routes
+Route::post('/user/create', [UserController::class, 'createUser']);
+Route::post('/admin/create', [UserController::class, 'createAdmin']);
+Route::post('/login', [UserController::class, 'login']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+
+// Event Routes
+Route::get('/events', [EventController::class, 'index']);
+Route::get('/event/{id}', [EventController::class, 'show']);
+Route::post('/event/create', [EventController::class, 'create']);
+Route::put('/event/update/{id}', [EventController::class, 'update']);
+Route::delete('/event/delete/{id}', [EventController::class, 'delete']);
+
+// Event Participant Routes
+Route::get('/participants', [EventParticipantController::class, 'index']);
+Route::get('/participant/{id}', [EventParticipantController::class, 'show']);
+Route::delete('/participant/delete/{id}', [EventParticipantController::class, 'delete']);
