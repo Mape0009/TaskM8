f107277ee85980c0985ae0e08a343a1e2102af53
Mape0009 <mape0009@edu.mercantec.dk>
2025-06-25 09:29:31 +0200

    .

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('role')->default('user');
             $table->date('birthdate')->nullable();
+            $table->boolean('logged_in')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
