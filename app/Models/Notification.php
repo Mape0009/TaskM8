cc58dfd67c7524fe4f9da2b77074b0d5a92987a4
Mape0009 <mape0009@edu.mercantec.dk>
2026-03-30 14:42:47 +0200

    Notification models, controller and migration

---
+++ app/Models/Notification.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    protected $fillable = [
+        'userId',
+        'eventId',
+        'message',
+        'isRead',
+    ];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class, 'eventId');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+}
